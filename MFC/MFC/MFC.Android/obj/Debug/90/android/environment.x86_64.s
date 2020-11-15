45e4e4123c9e07aca5ff5961f7351e0e819648d2
ValDagon <valdk652@gmail.com>
2020-11-15 12:54:54 +0500

    удалён мусор

--- MFC/MFC/MFC.Android/obj/Debug/90/android/environment.x86_64.s
+++ MFC/MFC/MFC.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"3c44ea0e-80ae-4caf-a5d8-aa6eb53e893e"
+	.asciz	"c5a39bbe-3eba-42b9-a4cb-17e75d0fe7b0"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
